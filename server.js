927d08df446e7bda9f084bb8e2f81d9b4169fb40
Nadav Julius <nadavjulius@gmail.com>
2020-04-05 15:28:49 +0300

    logger

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const path = require('path')
 const cors = require('cors')
+
 const app = express()
 const http = require('http').createServer(app);
 
@@ -22,4 +23,8 @@ if (process.env.NODE_ENV === 'production') {
     app.use(cors(corsOptions));
 }
 
-http.listen(port, () => console.log(`Marketing REST API listening on port ${port}!`))
+const logger = require('./services/logger.service')
+http.listen(port, () => {
+    logger.info('Server is running on port: ' + port)
+    console.log(`Marketing REST API listening on port ${port}!`)
+})
